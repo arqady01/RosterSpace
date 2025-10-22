4108483791bf95cbb5c3426ecf3af499da6fdbab
arqady01 <36727217+arqady01@users.noreply.github.com>
2025-10-22 12:04:11 +0800

    Initial Commit

---
+++ RosterSpaceTests/RosterSpaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  RosterSpaceTests.swift
+//  RosterSpaceTests
+//
+//  Created by mengfs on 10/22/25.
+//
+
+import Testing
+@testable import RosterSpace
+
+struct RosterSpaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
